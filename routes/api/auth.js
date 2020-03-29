02fdf81807bd10dde9109572c8469f505445fac4
lavlife06 <lav7990192369@gmail.com>
2020-03-29 13:22:25 +0530

    Added middleware to verify token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mwauth = require('../../middleware/mwauth');
+const User = require('../../models/User');
 
 // @route GET api/auth
 // desc   test route
 // access Public
 
-router.get('/', (req, res) => {
-  res.send('Auth route');
+router.get('/', mwauth, async (req, res) => {
+  try {
+    const userinfo = await User.findById(req.user.id).select('-password');
+    res.json(userinfo);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
