cf904e4e76f4aa51cfa9d3270a98e24f24a3f180
lavlife06 <lav7990192369@gmail.com>
2020-03-29 18:07:35 +0530

    done some changes in middleware

--- middleware/mwauth.js
+++ middleware/mwauth.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-    req.user = decoded.user;
+    req.user = decoded.user; // Now req.user will be acessed from anywhere
 
     next();
   } catch (err) {
